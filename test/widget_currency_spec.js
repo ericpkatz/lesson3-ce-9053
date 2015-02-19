b771b782b48a79bb6297199e4bf15d76e3358650
Eric Katz <ericpkatz@gmail.com>
2015-02-19 00:54:57 +0000

    can set properties in constructor

--- test/widget_currency_spec.js
+++ test/widget_currency_spec.js
@@ -1,8 +1,8 @@
 /* global describe it expect */
 
-var WidgetCurrency = function(){
-    this.base = "foo";
-    this.amount = 0;
+var WidgetCurrency = function(base, amount){
+    this.base = base || 'foo';
+    this.amount = amount || 0;
 };
 
 describe("WidgetCurrency", function(){
@@ -21,4 +21,24 @@ describe("WidgetCurrency", function(){
             expect(currency.amount).toEqual(0);
         });
     });
+    describe("setting values with constructor", function(){
+        describe("setting base to bar", function(){
+            it("base returns bar", function(){
+                var currency = new WidgetCurrency("bar");
+                expect(currency.base).toEqual("bar");
+            });
+        });
+        describe("setting amount to 18", function(){
+            it("amount is 18", function(){
+                var currency = new WidgetCurrency("bar", 18);
+                expect(currency.amount).toEqual(18);
+            });
+        });
+    });
+    
+    
+    
+    
 });
+
+
